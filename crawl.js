4d517fca6f380a86e8bdf5b16dfb6f42950a270f
Patrick Walsh <pmwals09@gmail.com>
2024-01-11 00:27:48 -0500

    add getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,15 +1,39 @@
+const { JSDOM } = require("jsdom");
+
 /**
-  * Accepts a valid URL and returns a normalized version of the URL with the following rules:
-  * - Remove trailing slashes
-  * - Remove the protocol
-  * @param {string} path - A valid URL
-  * @returns {string} A normalized version of the URL
-  */
+ * Accepts a valid URL and returns a normalized version of the URL with the following rules:
+ * - Remove trailing slashes
+ * - Remove the protocol
+ * @param {string} path - A valid URL
+ * @returns {string} A normalized version of the URL
+ */
 function normalizeURL(path) {
   const url = new URL(path);
-  return `${url.hostname}${url.pathname}`.replace(/\/$/, '');
+  return `${url.hostname}${url.pathname}`.replace(/\/$/, "");
+}
+
+/**
+ * Extract all of the URLs from a string of HTML
+ * @param {string} html- A string of HTML
+ * @param {string} baseURL - The base URL to use when making relative paths absolute
+ * @returns {string[]} An array of URLs
+ */
+function getURLsFromHTML(html, baseURL) {
+  const dom = new JSDOM(html);
+  const links = dom.window.document.querySelectorAll("a");
+  const urls = [];
+  for (let link of links) {
+    if (!link.href.startsWith("http")) {
+      const url = new URL(link.href, baseURL);
+      console.log(url.href);
+      link.href = url.href;
+    }
+    urls.push(link.href);
+  }
+  return urls;
 }
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect, describe } = require("@jest/globals");
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 
 describe("normalizeURL", () => {
   const expected = "blog.boot.dev/path";
@@ -14,5 +14,39 @@ describe("normalizeURL", () => {
   });
   test("http without trailing slash", () => {
     expect(normalizeURL("http://blog.boot.dev/path")).toBe(expected);
+  });
+});
+
+describe("getURLsFromHTML", () => {
+  const html = `
+  <html>
+    <head>
+      <title>My Blog</title>
+    </head>
+    <body>
+      <a href="http://blog.boot.dev/path/1">First Link</a>
+      <a href="http://blog.boot.dev/path/2">Second Link</a>
+      <a href="https://blog.boot.dev/path/3">Third Link</a>
+      <a href="/path/4">Fourth Link</a>
+    </body>
+  </html>
+  `;
+  test("Get all URLs from HTML", () => {
+    expect(getURLsFromHTML(html, "http://blog.boot.dev").length).toBe(4);
+  });
+  test("Gets an http link", () => {
+    expect(getURLsFromHTML(html, "http://blog.boot.dev")[1]).toBe(
+      "http://blog.boot.dev/path/2",
+    );
+  });
+  test("Gets a https link", () => {
+    expect(getURLsFromHTML(html, "http://blog.boot.dev")[2]).toBe(
+      "https://blog.boot.dev/path/3",
+    );
+  });
+  test("Make relative paths absolute", () => {
+    expect(getURLsFromHTML(html, "http://blog.boot.dev")[3]).toBe(
+      "http://blog.boot.dev/path/4",
+    );
   });
 });
