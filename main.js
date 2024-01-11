b2723cf020817557511c5aa6358afae6b5d13008
Patrick Walsh <pmwals09@gmail.com>
2024-01-11 00:33:00 -0500

    Initial main fn

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+function main() {
+  if (process.argv.length < 3) {
+    console.log("Please provide a base URL");
+    process.exit(1);
+  }
+
+  if (process.argv.length > 3) {
+    console.log("Please provide only one base URL");
+    process.exit(1);
+  }
+
+  const baseURL = process.argv[2];
+  console.log("Crawler starting at base URL: " + baseURL);
+}
+main();
